f6f25ba246cec5a6d214721ee7dd9484fa3b5d3c
Anna Steinhiler <anna.steinhilber@lyth.de>
2021-04-30 13:42:41 +0200

    Hide logout button on login fragment

--- app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -2,6 +2,7 @@ package com.udacity.shoestore.screens.login
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -12,6 +13,12 @@ import com.udacity.shoestore.databinding.LoginFragmentBinding
 class LoginFragment: Fragment() {
 
     private lateinit var binding: LoginFragmentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,5 +32,11 @@ class LoginFragment: Fragment() {
             false)
 
         return binding.root
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        val item = menu.findItem(R.id.logout_button)
+        item?.setVisible(false)
     }
 }
