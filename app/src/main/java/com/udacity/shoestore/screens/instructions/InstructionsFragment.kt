fefe00505291657c8a8c957c38faf07b41f6e5f3
Anna Steinhiler <anna.steinhilber@lyth.de>
2021-04-30 11:08:50 +0200

    Add navigation and fragments

---
+++ app/src/main/java/com/udacity/shoestore/screens/instructions/InstructionsFragment.kt
@@ -0,0 +1,29 @@
+package com.udacity.shoestore.screens.instructions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.InstructionFragmentBinding
+
+class InstructionsFragment: Fragment() {
+
+    private lateinit var binding: InstructionFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.instruction_fragment,
+            container,
+            false)
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
