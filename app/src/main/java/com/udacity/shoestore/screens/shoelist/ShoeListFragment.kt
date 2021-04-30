ed4609222700bbe0bb9934af4705a2dde19c6fd2
Anna Steinhiler <anna.steinhilber@lyth.de>
2021-04-30 18:59:18 +0200

    Create shoe list view model

--- app/src/main/java/com/udacity/shoestore/screens/shoelist/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoelist/ShoeListFragment.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.ShoeDetailFragmentBinding
 import com.udacity.shoestore.databinding.ShoeListFragmentBinding
+import com.udacity.shoestore.models.Shoe
 
 class ShoeListFragment : Fragment() {
 
-    private lateinit var binding: ShoeListFragmentBinding
+    private lateinit var viewModel: ShoeListViewModel
 
+    private lateinit var binding: ShoeListFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +31,28 @@ class ShoeListFragment : Fragment() {
             R.layout.shoe_list_fragment,
             container,
             false)
+        binding.lifecycleOwner = this
+
+        viewModel = ViewModelProvider(this, defaultViewModelProviderFactory)
+            .get(ShoeListViewModel::class.java)
+
+        binding.floatingActionButton.setOnClickListener {
+            viewModel.addShoe()
+        }
+
+        viewModel.shoeList.observe(viewLifecycleOwner, Observer {
+          // addListItem(it)
+            for (shoe in it) {
+                addListItem(shoe)
+            }
+        })
 
         return binding.root
+    }
+
+    private fun addListItem(shoe: Shoe) {
+        val subView = TextView(context)
+        subView.text = shoe.name
+        binding.linearLayout.addView(subView)
     }
 }
