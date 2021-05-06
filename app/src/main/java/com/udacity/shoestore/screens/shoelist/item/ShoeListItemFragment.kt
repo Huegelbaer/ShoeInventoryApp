20ed0a4a6eca15c883487b853f0bc38c4394ad88
Anna Steinhiler <anna.steinhilber@lyth.de>
2021-05-06 16:02:03 +0200

    Add shoe list item view

---
+++ app/src/main/java/com/udacity/shoestore/screens/shoelist/item/ShoeListItemFragment.kt
@@ -0,0 +1,45 @@
+package com.udacity.shoestore.screens.shoelist.item
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ShoeListItemFragmentBinding
+import com.udacity.shoestore.models.Shoe
+
+class ShoeListItemFragment: Fragment() {
+
+    private lateinit var viewModel: ShoeListItemViewModel
+    private lateinit var binding: ShoeListItemFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.shoe_list_item_fragment,
+            container,
+            false)
+
+/*
+        val shoe = arguments?.getParcelable<Shoe>("shoe")
+
+        val subViewModelFactory = ShoeListItemViewModelFactory(shoe!!)
+        viewModel = ViewModelProvider(this, subViewModelFactory)
+            .get(ShoeListItemViewModel::class.java)
+*/
+
+        viewModel = ViewModelProvider(this, defaultViewModelProviderFactory)
+            .get(ShoeListItemViewModel::class.java)
+
+        binding.viewModel = viewModel
+
+        return binding.root
+    }
+}
